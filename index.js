13edd2fe48620d89e56f9acdc3ea2e9d2345c8d3
Ashish Antil <ashishantil.antil07@gmail.com>
2024-02-22 21:15:56 +0530

    Fix reply bugs, and add Chalk

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const fs = require('fs');
 const rgb = require('./utils/rgb');
 const afk = require('./utils/afk');
 const Insta = require('@androz2091/insta.js');
+const { exec } = require('child_process');
+
+require('dotenv').config();
 
 const client = new Insta.Client();
 
-// cleaning runtime_logs.log
-fs.writeFileSync('./logs/runtime_logs.log', '');
+// cleaning runtime_logs.log and create logs directory if not present.
+fs.existsSync('./logs/') || fs.mkdirSync('./logs/');
+fs.writeFileSync('./logs/runtime_logs.log', '')
 
 function logger(...message){
   console.log(...message);
@@ -21,8 +25,11 @@ process.on('uncaughtException', (err) => {
 });
 
 (async () => {
-  const { green, yellow, blue, red, cyan, bgGreen, bgRed, bold } = (await import('chalk')).default;
+  const { green, yellow, blue, blueBright, red, cyan, bgGreen, bgRed, bold } = (await import('chalk')).default;
   const orange = rgb(255, 165, 0);
+
+  const BOT_PREFIX = process.env.BOT_PREFIX || '[BOT] ';
+  const COMMAND_PREFIX = process.env.COMMAND_PREFIX || '!';
 
 
   // On connection to Instagram.
@@ -30,17 +37,12 @@ process.on('uncaughtException', (err) => {
     logger(`\n${bgGreen('Connected to Instagram!')}\n  ${bold(orange('As:'))} ${client.user.username}\n`);
   })
 
-  const greet = ['hello', 'hi', 'aur basanti', 'hey', 'hola', 'namaste', 'hlo'];
+  const greet = ['Hello', 'Hi', 'Aur Basanti', 'Hey', 'Hola', 'Namaste', 'Hlo'];
 
-  let isBotInteraction = false;
 
   client.on('messageCreate', (message) => {
     
-    // On Message, log to console. format -> [MESSAGE] <time> <username>
-    logger(`\n${cyan('[MESSAGE] ')} ${green(Date.now())} ${message.author.username}`);
-
-    
-    const chatMessage = [new Date()]+' '+message.author.username+' '+message.content;
+    const chatMessage = `[`+new Date()+`]`+' '+message.author.username+' '+message.content;
 
     // Save to /chat/<username>.txt, if exists. else make file.
     fs.existsSync(`./chats/${message.chat.name || message.chat.id}.txt`) || fs.writeFileSync(`./chats/${message.chat.name || message.chat.id}.txt`, '');
@@ -49,14 +51,28 @@ process.on('uncaughtException', (err) => {
     // On successfully saving to file, log to console.
     logger(`${yellow('[INFO]')} Saved to file!\n`);
 
-    // If message is from bot or from owner, return.
-    if (message.author.id === client.user.id && !isBotInteraction) {
+    // If message is from bot or from owner.
+    if (message.author.id === client.user.id && message.content.startsWith(BOT_PREFIX)) return;
 
-      // message from owner and not bot. unset afk
-      afk.unset(message.chat.id);
+    // On Message, log to console. format -> [MESSAGE] <chat.name || chat.id> <time> <username>
+    logger(`\n${cyan('[MESSAGE] ')} ${blueBright(message.chat.name || message.chat.id)} ${green(Date.now())} ${message.author.username}`);
 
-      if(message.content === 'SET AFK' || message.content === 'SET AFK!') {
-        const isGlobal = message.content.includes('!');
+    if(message.author.id === client.user.id){
+      if(afk.isAFK(message.chat.id)){
+        logger(`${orange('[INFO]')} Unsetting AFK from ${message.chat.id}!`);
+        // message from owner and not bot. unset afk
+        afk.unset(message.chat.id);
+      }
+
+
+      // for commands
+
+      if(!message.content.startsWith(COMMAND_PREFIX)) return;
+
+      const command = message.content.slice(COMMAND_PREFIX.length).split(' ')[0].toUpperCase();  
+
+      if(command === 'SET-AFK' || command === 'GSET-AFK') {
+        const isGlobal = command.startsWith('G');
 
         if(isGlobal){
           afk.set('GLOBAL');
@@ -67,10 +83,10 @@ process.on('uncaughtException', (err) => {
         // log to console
         logger(`${orange('[INFO]')} AFK set!`);
 
-        message.chat.send('AFK set'+isGlobal?'!':'');
+        message.chat.sendMessage(BOT_PREFIX+'AFK set'+(isGlobal?'!':''));
       }
       return;
-    };
+    }
 
     // return if it's the first message in the chat.
     if (message.chat.messages.clone().array().length === 1) return;
@@ -78,21 +94,43 @@ process.on('uncaughtException', (err) => {
 
     // If author is not bot or owner, reply to the interaction.
 
-    isBotInteraction = true;
     // if chat in afk, reply with afk message.
     if (afk.isAFK(message.chat.id)) {
       logger(`${red('[INFO]')} Replying to afk!`);
-      message.chat.send('I am afk. I will reply when I am back.');
+      message.chat.sendMessage(BOT_PREFIX+'I am afk. I will reply when I am back.');
       return;
     }
 
     // greeting
-    if (greet.includes(message.content?.toLowerCase())) {
+    if (message.content && containsSubstrings(message.content, greet, false)) {
       logger(`${blue('[INFO]')} Replying to greeting!`);
-      message.chat.send(greet[Math.floor(Math.random() * greet.length)+' '+message.author.username]);
+      message.chat.sendMessage(BOT_PREFIX+greet[Math.floor(Math.random() * greet.length)]+' '+message.author.username);
+    }else if(containsSubstrings(message.content, ['thik hai', 'theek hai', 'ok'], false)){
+      logger(`${blue('[INFO]')} Replying to "ok"!`);
+      message.chat.sendMessage(BOT_PREFIX+':)');
     }
   });
 
   logger(`${yellow('Logging in...')}\n`);
+  // console.log(process.env.INSTA_ACCOUNT_USERNAME, process.env.INSTA_PASSWORD)
   client.login(process.env.INSTA_ACCOUNT_USERNAME, process.env.INSTA_PASSWORD);
 })();
+
+/**
+ * This function returns if any from a list of substrings is in the given string.
+ * @param {String} str The string to search in
+ * @param {Object} arr The array of substrings to search in given string
+ * @param {Boolean} matchCase Whether to match case or not.
+ * @returns {Boolean}
+ */
+const containsSubstrings = (str, arr, matchCase=false) => {
+  let found=false;
+  arr.every(elem => {
+    if((!matchCase?str.toLowerCase():str).includes(!matchCase?elem.toLowerCase():elem)){
+      found=true
+      return false
+    }
+    return true
+  })
+  return found
+}
